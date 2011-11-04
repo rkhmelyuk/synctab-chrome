98d535bed84aad83875031770d3078c419a3cb74
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-11-04 12:20:48 +0200

    High resolution icon.

--- package.sh
+++ package.sh
@@ -3,6 +3,7 @@
 rm -rf bin/
 zip synctab -x package.sh -r *
 mkdir bin
-mv synctab.zip bin/synctab.crx
+mv synctab.zip bin/synctab.zip
+cp bin/synctab.zip bin/synctab.crx
 
 echo 'SyncTab packaged.'
